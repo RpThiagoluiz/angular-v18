a37eea9f84835906a39eea66738b02b4c6e05aae
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2024-08-07 11:49:07 -0300

    feat: init project and register routes

---
+++ ticket-app/README.md
@@ -0,0 +1,5 @@
+Tickets
+
+## Note
+
+I use this site for do this project [SwaggerUI - free api](https://freeapi.miniprojectideas.com)
---
+++ ticket-app/src/app/pages/list-ticket/list-ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-ticket.component.html',
+  styleUrl: './list-ticket.component.css'
+})
+export class ListTicketComponent {
+
+}
